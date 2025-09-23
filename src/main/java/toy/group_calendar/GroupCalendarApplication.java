6df5023a19cfac6fc05851d57c4d511420b9134b
shc6260 <shc6260@gmail.com>
2025-09-23 20:00:39 +0900

    프로젝트 생성

---
+++ src/main/java/toy/group_calendar/GroupCalendarApplication.java
@@ -0,0 +1,13 @@
+package toy.group_calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupCalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toy/group_calendar/GroupCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.group_calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
